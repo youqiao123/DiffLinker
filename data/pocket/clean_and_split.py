4e87b94b1f727685b36c98db548a50d2bc85cf80
youqiao123 <qianyouqiao@outlook.com>
2025-09-27 23:10:49 +0800

    update

--- data/pocket/clean_and_split.py
+++ data/pocket/clean_and_split.py
@@ -4,6 +4,7 @@ sys.path.append('../../')
 import argparse
 import os
 import subprocess
+from pathlib import Path
 
 from rdkit import Chem
 from tqdm import tqdm
@@ -17,6 +18,49 @@ def get_relevant_ligands(mol):
         if 10 < frag.GetNumAtoms() <= 40:
             ligands.append(frag)
     return ligands
+
+def process_pdbbind_protein(input_dir, proteins_dir):
+    root = Path(input_dir)
+    proteins_path = Path(proteins_dir)
+    proteins_path.mkdir(parents=True, exist_ok=True) # 允许递归创建父目录
+
+    for protein_path in tqdm(root.rglob('*_protein.pdb')):
+        pdb_code = protein_path.stem[:4]
+
+        temp_path_0 = proteins_path / f'{pdb_code}_temp_0.pdb'
+        temp_path_1 = proteins_path / f'{pdb_code}_temp_1.pdb'
+
+        out_protein_path = proteins_path / f'{pdb_code}_protein.pdb'
+
+        subprocess.run(f'pdb_selmodel -1 {protein_path} > {temp_path_0}', shell=True)
+        subprocess.run(f'pdb_delelem -H {temp_path_0} > {temp_path_1}', shell=True)
+        subprocess.run(f'pdb_delhetatm {temp_path_1} > {out_protein_path}', shell=True)
+
+        temp_path_0.unlink()
+        temp_path_1.unlink()
+
+def process_pdbbind_ligand(input_dir, ligands_dir):
+    root = Path(input_dir)
+    ligands_path = Path(ligands_dir)
+    ligands_path.mkdir(parents=True, exist_ok=True) # 允许递归创建父目录
+
+    for ligand_path in tqdm(root.rglob('*_ligand.sdf')):
+        pdb_code = ligand_path.stem[:4]
+
+        try:
+            mol = Chem.SDMolSupplier(str(ligand_path), sanitize=False)[0]
+        except Exception as e:
+            print(f'Problem reading ligands PDB={pdb_code}: {e}')
+            continue
+
+        out_ligand_path = ligands_path / f'{pdb_code}_0.mol'
+        Chem.MolToMolFile(mol, str(out_ligand_path))
+
+
+def run_pdbbind(input_dir, proteins_dir, ligands_dir):
+    process_pdbbind_protein(input_dir, proteins_dir)
+    process_pdbbind_ligand(input_dir, ligands_dir)
+    print('PDBBind processing completed.')
 
 
 def run(input_dir, proteins_dir, ligands_dir):
@@ -57,7 +101,8 @@ def run(input_dir, proteins_dir, ligands_dir):
                 continue
 
             try:
-                ligands = get_relevant_ligands(mol)
+                # 为什么要有这步操作？问一下huifeng师兄
+                ligands = get_relevant_ligands(mol) 
             except Exception as e:
                 print(f'Problem getting relevant ligands PDB={pdb_code}: {e}')
                 continue
@@ -75,4 +120,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     disable_rdkit_logging()
-    run(input_dir=args.in_dir, proteins_dir=args.proteins_dir, ligands_dir=args.ligands_dir)
+    # run(input_dir=args.in_dir, proteins_dir=args.proteins_dir, ligands_dir=args.ligands_dir)
+    run_pdbbind(input_dir=args.in_dir, proteins_dir=args.proteins_dir, ligands_dir=args.ligands_dir)
